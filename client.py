f2765665f493ef19f4b22181d7be49ddc5d0308e
Mariamfathyy <mariamfathy000123@gmail.com>
2025-03-26 15:06:26 +0200

    Added client file

---
+++ client.py
@@ -0,0 +1,33 @@
+import threading
+import socket
+NICKNAME = input('Choose an Nickname')
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((socket.gethostname(), 12345))
+
+
+def client_receive():
+    while True:
+        try:
+            message = client.recv(1024).decode('ascii')
+            if message == "NICKNAMES":
+                client.send(NICKNAME.encode('ascii'))
+            else:
+                print(message)
+        except:
+            print('Error!')
+            client.close()
+            break
+
+def client_send():
+    while True:
+        try:
+            message= '{}:{}'.format(NICKNAME,input(" "))
+            client.sendall(message.encode('ascii'))
+            print("send correctly")
+        except:
+            print("Error")
+
+receive_thread = threading.Thread(target=client_receive)
+receive_thread.start()
+send_thread=threading.Thread(target=client_send)
+send_thread.start()
