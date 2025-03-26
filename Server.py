3649a6fd12f53743ffa89c3551d22ed0e1a3504d
FT507 <mohamedezio57@gmail.com>
2025-03-26 11:22:14 +0200

    Deleted unnecessary comments (bl balady feh badehyat m4 lazem 2aket kol satr comment  ma3loma lba3den)
    added 2 function to start receiving and threading
    (receive(),start())

--- Server.py
+++ Server.py
@@ -1,15 +1,51 @@
 import socket
-serv=socket.socket(socket.AF_INET,socket.SOCK_STREAM) #To create a new socket object
+serv=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 import threading                                      #To make multiple clients connect simultaneously
-host=socket.gethostname()                             #To get the server's hostname
-port=12345                                            #To identify port for the server
-serv_add=(host,port)                                  #To store the host and port in a tuple as server address
-serv.bind(serv_add)                                   #To bind the server to the specified host and port
-serv.listen()                                         #Waiting for clients to connect.....
-members=[]                                            #To store all connected client sockets
-usernames=[]                                          #To store the usernames of connected clients
+host=socket.gethostname()
+port=12345
+serv_add=(host,port)
+serv.bind(serv_add)
+serv.listen(10)
+members=[]
+usernames=[]
 
 ### We built this function to send messages to all clients in the chatroom ###
-def broadcast(tosend):
-    for mb in members:                                #To loop over each connected client
-        mb.send(tosend.encode())                      #To Send the message to that client
+def broadcast(sendmsg):
+    for mb in members:
+        mb.send(sendmsg.encode())
+
+
+#Receive message for client , detect connection failed , target for threading
+def receive(member):
+    #Remeber we implement IN_LOOP as receiving not called for on time and broke it in case of fail in connection
+    while True:
+        try:
+            msg = member.recv(1024)
+            broadcast(msg.decode())
+        except:
+            print("ERROR 404")
+            Index = members.index(member)
+            username = usernames[Index]
+            member.recv(1024)
+            broadcast(username + "IS REMOVED")
+            usernames.remove(username)
+        break
+
+
+## Created to start threading between client and server
+def start():
+    member , addr  = serv.accept()
+    member.send("NICKNAMES".encode())
+    userNames = member.recv(1024).decode()
+    ## add more member without effecting on older ones
+    userNames.append(userNames)
+    members.append(members)
+    broadcast(userNames)
+    thread = threading.Thread(target=receive,args=(userNames,))  #The target is receive() function to get messages simultaneously
+    thread.start()
+    print("Server is waiting")
+
+start()
+
+ # def server():
+
